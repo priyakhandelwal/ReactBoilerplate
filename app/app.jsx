bc376a94ab10a1baa44c67255a7b621ea552768c
prkhandelwal <prkhandelwal@paypal.com>
2016-08-22 00:07:48 +0530

    Object spread feature

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var temp = {
+	"firstName": "Priya"
+};
+
+var obj = {
+	"lastName": "Khandelwal",
+	...temp
+};
+console.log(obj);
+
 ReactDOM.render(
 	<h1>Boilerplate</h1>,
   document.getElementById('app')
--- public/bundle.js
+++ public/bundle.js
@@ -46,13 +46,24 @@
 
 	'use strict';
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(34);
 
+	var temp = {
+		"firstName": "Priya"
+	};
+
+	var obj = _extends({
+		"lastName": "Khandelwal"
+	}, temp);
+	console.log(obj);
+
 	ReactDOM.render(React.createElement(
-	  'h1',
-	  null,
-	  'Boilerplate'
+		'h1',
+		null,
+		'Boilerplate'
 	), document.getElementById('app'));
 
 /***/ },

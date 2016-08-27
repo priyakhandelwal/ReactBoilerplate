1258f484de8ab3806bccea99fd95ee03964df20a
prkhandelwal <prkhandelwal@paypal.com>
2016-08-27 17:13:02 +0530

    Grid system and styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h1>Examples</h1>
-      <p>Welecome to the examples page!</p>
+      <h1 className="text-centered">Examples</h1>
+      <p>Here are the links to few examples. Fell free to try these out.</p>
+      <ol>
+        <li>
+          <Link to="/?location='Philadelphia'">Philadelphia</Link>
+        </li>
+        <li>
+          <Link to="/?location='Rio'">Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
 
   );
